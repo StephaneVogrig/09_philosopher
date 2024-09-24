e646ad4666bf1cbe3c1b751c6d6b354ebd9a54ad
Stephane Vogrig <svogrig@1B5.42angouleme.fr>
2024-09-25 01:29:27 +0200

    fix exit code

--- philo/src/exit.c
+++ philo/src/exit.c
@@ -6,7 +6,7 @@
 /*   By: svogrig <svogrig@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/02 20:04:01 by svogrig           #+#    #+#             */
-/*   Updated: 2024/06/19 15:05:32 by svogrig          ###   ########.fr       */
+/*   Updated: 2024/09/24 22:34:46 by svogrig          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,13 +15,13 @@
 void	exit_on_invalid_argument(char *str)
 {
 	printf("philo: \'%s\' invalid argument\n", str);
-	exit (SUCCESS);
+	exit (EXIT_SUCCESS);
 }
 
 void	exit_on_overflow(char *str)
 {
 	printf("philo: \'%s\' out of range [0 - %u\n", str, UINT_MAX);
-	exit (SUCCESS);
+	exit (EXIT_SUCCESS);
 }
 
 void	exit_on_nbr_arg(void)
@@ -29,5 +29,5 @@ void	exit_on_nbr_arg(void)
 	write(1, "usage: <number_of_philosopher> <time_to_die> ", 45);
 	write(2, "<time_to_eat> <time_to_sleep> ", 30);
 	write(2, "[number_of_times_each_philosopher_must_eat]\n", 44);
-	exit (SUCCESS);
+	exit (EXIT_SUCCESS);
 }
